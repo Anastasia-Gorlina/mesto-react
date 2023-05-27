ebef5e75e6d9c186d1ba39596ad04d74f03cc636
Anastasia Gorlina <gobi199@yandex.ru>
2023-05-27 15:32:40 +0300

    deleted

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header className="header">
-            <img className="header__logo" src={require('../images/Logo.svg').default} alt="Site name" />
-        </header>
-        )
-    }
-        
-export default Header;
